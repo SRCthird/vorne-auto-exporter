77db6ff09fbeb5547ee2344c053fb123d251ca45
SRCthird <SRCthird@gmail.com>
2023-08-24 21:45:47 -0400

    Defined the MySQL insert function.

--- processQuery.js
+++ processQuery.js
@@ -1,8 +1,16 @@
-const query = require('query');
-import Mapping from './mapping.js';
-const mysql = require('mysql');
+import { useQuery } from './useQuery.js';
+import { Mapping } from './mapping.js';
+import mysql from 'mysql';
 
-const processQuery = (Database, server, Table) => {
+export const processQuery = async (Database, server, Table) => {
+
+    function formatTime(timestamp) {
+        const baseDate = new Date(1900, 0, 1);
+        const secondsToAdd = parseFloat(timestamp);
+        baseDate.setSeconds(baseDate.getSeconds() + secondsToAdd);
+        return baseDate;
+    }
+
     console.log(`Processing ${Table}`);
 
     const Limit = 100
@@ -12,30 +20,30 @@ const processQuery = (Database, server, Table) => {
     const Columns = Object.keys(Database['Tables']['Streams'][Table]['columns']);
 
     const columnTransformationsMap = {
-        'int': int,
-        'float': float,
-        'str': str,
+        'int': parseInt,
+        'float': parseFloat,
+        'str': String,
         'formatTime': formatTime,
-        'mapping.state': mapping.state,
-        'mapping.reason1': mapping.reason1,
-        'mapping.reason2': mapping.reason2,
-        'mapping.reason3': mapping.reason3,
-        'mapping.reason4': mapping.reason4,
-        'mapping.reason5': mapping.reason5,
-        'mapping.reason6': mapping.reason6,
-        'mapping.error_code': mapping.error_code,
-        'mapping.channel_state': mapping.channel_state,
-        'mapping.process_timer_state': mapping.process_timer_state,
-        'mapping.production_state': mapping.production_state,
-        'mapping.production_state_internal': mapping.production_state_internal,
-        'mapping.tcp_state': mapping.tcp_state,
-        'mapping.toggle_switch': mapping.toggle_switch,
-        'mapping.digital_display': mapping.digital_display,
-        'mapping.run_mode': mapping.run_mode,
-        'mapping.serial_mode': mapping.serial_mode,
-        'mapping.sink_source': mapping.sink_source,
-        'mapping.display_type': mapping.display_type,
-        'mapping.event_type': mapping.event_type
+        'mapping.state': mapping.state.bind(mapping),
+        'mapping.reason1': mapping.reason1.bind(mapping),
+        'mapping.reason2': mapping.reason2.bind(mapping),
+        'mapping.reason3': mapping.reason3.bind(mapping),
+        'mapping.reason4': mapping.reason4.bind(mapping),
+        'mapping.reason5': mapping.reason5.bind(mapping),
+        'mapping.reason6': mapping.reason6.bind(mapping),
+        'mapping.error_code': mapping.error_code.bind(mapping),
+        'mapping.channel_state': mapping.channel_state.bind(mapping),
+        'mapping.process_timer_state': mapping.process_timer_state.bind(mapping),
+        'mapping.production_state': mapping.production_state.bind(mapping),
+        'mapping.production_state_internal': mapping.production_state_internal.bind(mapping),
+        'mapping.tcp_state': mapping.tcp_state.bind(mapping),
+        'mapping.toggle_switch': mapping.toggle_switch.bind(mapping),
+        'mapping.digital_display': mapping.digital_display.bind(mapping),
+        'mapping.run_mode': mapping.run_mode.bind(mapping),
+        'mapping.serial_mode': mapping.serial_mode.bind(mapping),
+        'mapping.sink_source': mapping.sink_source.bind(mapping),
+        'mapping.display_type': mapping.display_type.bind(mapping),
+        'mapping.event_type': mapping.event_type.bind(mapping)
     }
 
     const columnTransformations = Columns.map(column => columnTransformationsMap[Database['Tables']['Streams'][Table]['columns'][column]]);
@@ -46,15 +54,13 @@ const processQuery = (Database, server, Table) => {
     const columnsString = Columns.join(', ');
     const sqlStatement = `SELECT ${columnsString} FROM ${Table} ORDER BY ${Columns[0]} DESC LIMIT ${Limit};`;
 
-    const resultQuery = query(server, sqlStatement, columnTransformations, debug, raw);
-
-    console.log(resultQuery);
- /*   
+    const resultQuery = await useQuery(server, sqlStatement, columnTransformations, debug, raw);
+    
     // Create a connection to the MySQL database
     const connection = mysql.createConnection({
         host: 'localhost',
-        user: 'root',
-        password: '$TephenC03',
+        user: 'Administrator',
+        password: 'aragorn',
         database: Database['Name']
     });
 
@@ -73,12 +79,10 @@ const processQuery = (Database, server, Table) => {
             if (err) {
                 console.error('Failed to insert:', err);
             }
-            // ... any other handling, like logging successful insertions ...
         });
     });
 
     connection.end();
-*/
 }
 
 export default processQuery;
