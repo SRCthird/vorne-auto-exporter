2f88064bd380d43ed36cca05cdfb38dfb5de8c09
SRCthird <SRCthird@gmail.com>
2023-08-25 16:23:10 -0400

    Created single query and looped query

--- src/recurrance.js
+++ src/recurrance.js
@@ -1,21 +1,28 @@
-import fs from 'fs';
-import yaml from 'js-yaml';
+import query from '../modules/query.js';
 
-import('../modules/processQuery.js').then(({ processQuery }) => {
+// Loading spinner logic
+let chars = ['|', '/', '-', '\\'];
+let currentChar = 0;
 
-    // Read the schema.yaml file
-    const fileContent = fs.readFileSync('schema.yaml', 'utf8');
+function showSpinner() {
+    process.stdout.write(`\r${chars[currentChar]} Waiting for next execution...`);
+    currentChar = (currentChar + 1) % chars.length;
+}
 
-    // Parse the YAML content
-    const config = yaml.load(fileContent);
+// Function to run the main program, wait 24 hours, and then run it again
+function runAndWait() {
+    query();
 
-    for (const [database, content] of Object.entries(config['Database'])) {
-        console.log(`Processing on server ${database}`);
-        
-        const Database = config['Database'][database];
-        // Process each table in the Stream section
-        for (const Table of Object.keys(Database['Tables']['Streams'])) {
-            processQuery(Database, database, Table);
-        }
-    }
-});
+    // Start the spinner
+    let spinnerInterval = setInterval(showSpinner, 200);
+
+    // Stop the spinner after 24 hours and run the program again
+    setTimeout(() => {
+        clearInterval(spinnerInterval);
+        process.stdout.write('\rDone waiting!     \n'); // Clear the spinner
+        runAndWait(); // recursive call
+    }, 24 * 60 * 60 * 1000);
+}
+
+// Initial call
+runAndWait();
